dca62611bfcc2aff0185456fe893fc633c8c8d1b
akdntmrkdjqtsp <gytjd9516@naver.com>
2023-03-11 15:59:05 +0900

    1과제까지

---
+++ carlist/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Car from './car';
+function App() {
+  return (
+    <div >
+      <Car/>
+    </div>
+  );
+}
+
+export default App;
---
+++ routerexample/src/pages/nopage.js
@@ -0,0 +1,4 @@
+const NoPage = () => {
+    return <h1>No Page</h1>;
+};
+export default NoPage;
