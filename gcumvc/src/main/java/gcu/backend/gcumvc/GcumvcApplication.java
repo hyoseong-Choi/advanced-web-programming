09b0e537158c635733ab41e244e9dd78042cd276
akdntmrkdjqtsp <gytjd9516@naver.com>
2023-03-29 14:46:55 +0900

    스프링 mvc까지

---
+++ gcumvc/src/main/java/gcu/backend/gcumvc/GcumvcApplication.java
@@ -0,0 +1,13 @@
+package gcu.backend.gcumvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcumvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcumvcApplication.class, args);
+	}
+
+}
---
+++ gcumvc/src/test/java/gcu/backend/gcumvc/GcumvcApplicationTests.java
@@ -0,0 +1,13 @@
+package gcu.backend.gcumvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcumvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
