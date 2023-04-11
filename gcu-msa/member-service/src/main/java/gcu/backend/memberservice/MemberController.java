b29965b0c6d8b1c841ca900f0008a1b34fc899fa
DESKTOP-7MNK8JV\HP <gytjd9516@naver.com>
2023-04-11 22:39:17 +0900

    스프링 jpa까지

--- gcu-msa/member-service/src/main/java/gcu/backend/memberservice/MemberController.java
+++ gcu-msa/member-service/src/main/java/gcu/backend/memberservice/MemberController.java
@@ -1,13 +1,35 @@
 package gcu.backend.memberservice;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class MemberController {
-    @GetMapping("/member")
-    public Member getMember() {
-        return new Member(1L, "Gachon", "gcu");
+    final MemberRepository memberRepository;
+
+    public MemberController(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
-    
+
+    @PostMapping("/api/member")
+    public String create(@RequestBody Member member) {
+        memberRepository.save(member);
+        return "create ok!";
+    }
+
+    @GetMapping("/api/member/{id}")
+    public Optional<Member> show(@PathVariable("id") Long id) {
+        return memberRepository.findById(id);
+    }
+
+    @GetMapping("/api/member")
+    public List<Member> showAll() {
+        return memberRepository.findAll();
+    }
+
+
 }
